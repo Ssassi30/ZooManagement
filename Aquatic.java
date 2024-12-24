fb78354ab070a33742c9270b858574bb8dcfd120
Ssassi30 <126425782+Ssassi30@users.noreply.github.com>
2024-12-24 18:07:46 +0100

    Prosit5

---
+++ Aquatic.java
@@ -0,0 +1,26 @@
+package tn.esprit.gestionzoo.entities;
+
+public class Aquatic extends Animal {
+    String habitat;
+
+
+    public Aquatic(String family, String name, int age, boolean isMammal,String habitat) {
+        super(family, name, age, isMammal);
+        this.habitat = habitat;
+    }
+    public String getHabitat() {
+        return habitat;
+    }
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + ", habitat=" + habitat;
+    }
+
+    public void swim()
+    {
+        System.out.println("This aqutic animal is swimming");
+    }
+}
